fc77da2a8964553866e24d6c510e14ff94b70e23
4m4n <aman.singh.rke@gmail.com>
2017-07-11 21:23:12 -0400

    Heap implementation
    Heap Basics:
    https://www.youtube.com/watch?v=t0Cq6tVNRBA

---
+++ Heap.js
@@ -0,0 +1,93 @@
+var minHeap = function(){
+  this.items = [];
+}
+
+minHeap.prototype = {
+  getSize: function(){
+    return this.items.length;
+  },
+  getLeftChildIndex: function(parentIndex){
+    return 2*parentIndex + 1;
+  },
+  getRightChildIndex: function(parentIndex){
+    return 2*parentIndex + 2;
+  },
+  getParentIndex: function(childIndex){
+    return Math.floor((childIndex - 1)/2);
+  },
+  hasLeftChild: function(index){
+    if(this.getLeftChildIndex(index) < this.getSize())
+      return true;
+    return false;
+  },
+  hasRightChild: function(index){
+    if(this.getRightChildIndex(index) < this.getSize())
+      return true;
+    return false;
+  },
+  hasParent: function(index){
+    if(this.getParentIndex(index) >= 0)
+      return true;
+    return false;
+  },
+  leftChild: function(index){
+    return this.items[this.getLeftChildIndex(index)];
+  },
+  rightChild: function(index){
+    return this.items[this.getRightChildIndex(index)]
+  },
+  parent: function(index){
+    return this.items[this.getParentIndex(index)];
+  },
+  swap: function(index1, index2){
+    var temp = this.items[index1];
+    this.items[index1] = this.items[index2];
+    this.items[index2] = temp;
+  },
+  peek: function(){
+    if(this.getSize() <= 0)
+      throw "Heap is empty.";
+    
+    return this.items[0];
+  },
+  poll: function(){
+    if(this.getSize() <= 0)
+      throw "Heap is empty.";
+    
+    var item = this.items[0];
+    
+    this.items[0] = this.items.pop();
+    
+    this.heapifyDown();
+    
+    return item;
+  },
+  add: function(item){
+    this.items.push(item);
+    this.heapifyUp();
+  },
+  heapifyUp: function(){
+    var index = this.getSize() - 1;
+    
+    while(this.hasParent(index) && this.parent(index) > this.items[index]){
+      var parentIndex = this.getParentIndex(index)
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  },
+  heapifyDown: function(){
+    var index = 0;
+    while(this.hasLeftChild(index)){
+      var smallerChildIndex = this.getLeftChildIndex(index);
+      if(this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index))
+        smallerChildIndex = this.getRightChildIndex(index);
+
+      if(this.items[index] < this.items[smallerChildIndex])
+        break;
+      else
+        this.swap(index, smallerChildIndex);
+      
+      index = smallerChildIndex;
+    }
+  }
+};
